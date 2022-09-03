5e469656465daa770c9ba3ebd83ea2cb54f6a95f
Alan Yanse <yansealan@gmail.com>
2022-09-03 19:44:31 -0300

    agregando funcionalidad al login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+  loading: boolean = false;
+
+  constructor( private formBuilder: FormBuilder) { 
+    this.formGroup = this.formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+
+  ingresar(): void{
+    
+    //console.log(this.formGroup);
+
+    const email = this.formGroup.value.email;
+    const password = this.formGroup.value.password;
+
+    // fake credentials
+
+    const fakeEmail = "alkemy@alkemy.com";
+    const fakePass = "react";
+
+    // validación
+
+
+    if(fakeEmail === email && fakePass === password){
+      
+      this.fakeLoading();
+      
+    }else{
+
+      alert("Alguna de las credenciales ingresadas no es válida");
+      this.formGroup.reset();
+    }
+  }
+
+  fakeLoading(): void{
+
+    this.loading = true;
+    setTimeout(()=>{
+      this.loading = false;
+    }, 2500);
+  }
+
+
 
   ngOnInit(): void {
   }
