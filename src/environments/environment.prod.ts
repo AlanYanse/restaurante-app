a863804c915f1dfd180fc1763b433c092909e340
Alan Yanse <yansealan@gmail.com>
2022-09-13 16:05:11 -0300

    actualizando para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://api.spoonacular.com/",
+  apiKey: "1d99dcbc293c45f2a253d9713b0f60f3",
+  urlLogin: "http://challenge-react.alkemy.org/"
 };
